dbcda622cbdac021143fea83733fb2578a25f05c
Liza Panchenko <panchenko.elisa@gmail.com>
2022-05-18 14:17:35 +0100

    Meals components created

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import MealsImg from "../../assets/mealsHeaderImage.jpg";
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 export default function Header() {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={MealsImg} alt="A table full of delicious food!"></img>
       </div>
     </React.Fragment>
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+
+export default function AvailableMeals() {
+  return <div className={classes.meals}></div>;
+}
